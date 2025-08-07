da87ee679946e52889d289a08831a6580121057d
Esam4c <esamunimail@gmail.com>
2025-08-07 03:49:19 +0100

    iterativeResolveText first atteempt

--- src/Resolver.java
+++ src/Resolver.java
@@ -214,13 +214,126 @@ public class Resolver implements ResolverInterface {
             int queriesSent = 0;
             while (queriesSent < 20) {
                 System.out.println("Querying server: " + nextServerToQuery.getHostAddress() + "for the TXT record of " + currentDomainName);
+
+                ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
+                DataOutputStream dataStream = new DataOutputStream(byteStream);
+
+                short transactionID = (short) new Random().nextInt();
+                dataStream.writeShort(transactionID);
+                dataStream.writeShort(0x0000);
+                dataStream.writeShort(1);
+                dataStream.writeShort(0);
+                dataStream.writeShort(0);
+                dataStream.writeShort(0);
+
+                String[] domainParts = currentDomainName.split("\\.");
+                for (String part : domainParts) {
+                    byte[] partBytes = part.getBytes("UTF-8");
+                    dataStream.writeByte(partBytes.length);
+                    dataStream.write(partBytes);
+                }
+                dataStream.writeByte(0);
+                //txt record
+                dataStream.writeShort(16);
+                dataStream.writeShort(1);
+
+                byte[] queryBytes = byteStream.toByteArray();
+                DatagramSocket socket = new DatagramSocket();
+                DatagramPacket queryPacket = new DatagramPacket(queryBytes, queryBytes.length, nextServerToQuery, rootServerPort);
+                socket.send(queryPacket);
+
+                byte[] responseBuffer = new byte[1024];
+                DatagramPacket responsePacket = new DatagramPacket(responseBuffer, responseBuffer.length);
+                socket.receive(responsePacket);
+                socket.close();
+
+                ByteArrayInputStream responseByteStream = new ByteArrayInputStream(responseBuffer);
+                DataInputStream responseDataStream = new DataInputStream(responseByteStream);
+                short responseID = responseDataStream.readShort();
+                if (responseID != transactionID) {
+                    throw new Exception("ERROR! TransactionID mismatch");
+                }
+                responseDataStream.readShort();
+                short questions = responseDataStream.readShort();
+                short answerCount = responseDataStream.readShort();
+                short authorityCount = responseDataStream.readShort();
+                short additionalCount = responseDataStream.readShort();
+
+                for (int j = 0; j < questions; j++) {
+                    int labelSize;
+                    do {
+                        labelSize = responseDataStream.readUnsignedByte();
+                        if (labelSize > 0) {
+                            responseDataStream.skipBytes(labelSize);
+                        }
+                    } while (labelSize != 0);
+                    responseDataStream.skipBytes(4);
+                }
+
+                if (answerCount > 0) {
+                    boolean foundCname = false;
+                    for (int j = 0; j < answerCount; j++) {
+                        String recordName = helperReadName(responseDataStream, responseBuffer);
+                        short resourceRecordType = responseDataStream.readShort();
+                        responseDataStream.readShort();
+                        responseDataStream.readInt();
+                        short resourceDataLength = responseDataStream.readShort();
+
+                        // CHANGE #2: Look for a TXT record (type 16)
+                        if (resourceRecordType == 16) {
+                            int txtLength = responseDataStream.readByte();
+                            byte[] txtBytes = new byte[txtLength];
+                            responseDataStream.readFully(txtBytes);
+                            return new String(txtBytes, "UTF-8");
+                        } else if (resourceRecordType == 5) { // CNAME Record
+                            currentDomainName = helperReadName(responseDataStream, responseBuffer);
+                            foundCname = true;
+                            break;
+                        } else {
+                            responseDataStream.skipBytes(resourceDataLength);
+                        }
+                    }
+                    if (foundCname) {
+                        break;
+                    }
+                } else if (authorityCount > 0) {
+                    // This referral logic is identical to the method above
+                    Map<String, InetAddress> glueRecords = new HashMap<>();
+                    List<String> nsNames = new ArrayList<>();
+                    for (int j = 0; j < authorityCount + additionalCount; j++) {
+                        String recordName = helperReadName(responseDataStream, responseBuffer);
+                        short recordType = responseDataStream.readShort();
+                        responseDataStream.readShort();
+                        responseDataStream.readInt();
+                        short dataLength = responseDataStream.readShort();
+                        if (recordType == 2) {
+                            String nsDomainName = helperReadName(responseDataStream, responseBuffer);
+                            nsNames.add(nsDomainName);
+                        } else if (recordType == 1) {
+                            byte[] ipBytes = new byte[dataLength];
+                            responseDataStream.readFully(ipBytes);
+                            glueRecords.put(recordName, InetAddress.getByAddress(ipBytes));
+                        } else {
+                            responseDataStream.skipBytes(dataLength);
+                        }
+                    }
+                    boolean foundNextServer = false;
+                    for (String nsName : nsNames) {
+                        if (glueRecords.containsKey(nsName)) {
+                            nextServerToQuery = glueRecords.get(nsName);
+                            foundNextServer = true;
+                            break;
+                        }
+                    }
+                    if (foundNextServer) {
+                        queriesSent++;
+                        continue;
+                    }
+                }
+                throw new Exception("ERROR! No answer and no valid referral found!");
             }
         }
-
-
-
-
-        throw new Exception("Not implemented");
+        return null; //returns null if no txt record
     }
 
     public String iterativeResolveName(String domainName, int type) throws Exception {
